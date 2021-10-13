eadbca3fb3f389837d7758eaf2c667e01b64c58f
Doran Kayoumi <dorankayoumi@gmail.com>
2021-10-13 16:34:38 +0200

    Update db helper

--- site/html/database.php
+++ site/html/database.php
@@ -3,22 +3,25 @@
 
 class Database {
 
-    private $db;
+    private $conn;
     
     function __construct() {
-        var_dump('waaaaaaaaaat');
         // Create (connect to) SQLite database in file
-        $this->db = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
+        $this->conn = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
         // Set errormode to exceptions
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, 
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, 
                             PDO::ERRMODE_EXCEPTION);
     }
 
     public function find_user_by_id($id) {
-        return $this->db->query("SELECT * FROM users WHERE id = '{$id}'")->fetchAll();
+        return $this->conn->query("SELECT * FROM users WHERE id = '{$id}'")->fetchAll()[0];
     }
 
     public function find_user_by_username($username) {
-        return $this->db->query("SELECT * FROM users WHERE username = '{$username}'")->fetchAll();
+        return $this->conn->query("SELECT * FROM users WHERE username = '{$username}'")->fetchAll();
+    }
+
+    public function get_users() {
+        return $this->conn->query("SELECT * FROM users")->fetchAll();
     }
 }
