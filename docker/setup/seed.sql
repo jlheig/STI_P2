27c051f83b6f85b521d68e53891815ff84b515e9
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-10-14 15:01:28 +0200

    Added messages in the seed.sql files

--- docker/setup/seed.sql
+++ docker/setup/seed.sql
@@ -1,3 +1,6 @@
 INSERT INTO users (username, password, role) VALUES ('root', '63a9f0ea7bb98050796b649e85481845', 'admin');
 INSERT INTO users (username, password) VALUES ('dany', '1b9fc02e98389d29c1506fe944b07d16');
 INSERT INTO users (username, password, active) VALUES ('zebra', '69c459dd76c6198f72f0c20ddd3c9447', 0);
+INSERT INTO emails (sender, receiver, subject, message, received_date) VALUES ('dany', 'zebra', 'Hello, World!', 'Hello there general Kenobi', '14-10-2021 15:00:00');
+INSERT INTO emails (sender, receiver, subject, message, received_date) VALUES ('zebra', 'dany', 'Hello, World!', 'Hello there general Kenobi', '14-10-2021 15:15:00');
+INSERT INTO emails (sender, receiver, subject, message, received_date) VALUES ('root', 'dany', 'Hello, World!', 'Hello there general Kenobi', '14-10-2021 15:20:00');
