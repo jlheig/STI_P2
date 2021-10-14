80c597ac54b3cd8aaf2aacc6a184d5ed3d753317
Doran Kayoumi <dorankayoumi@gmail.com>
2021-10-14 23:03:38 +0200

    Make admin page pretty

--- site/html/admin.php
+++ site/html/admin.php
@@ -21,36 +21,38 @@ require_once('includes/header.php');
 <!-- Navigation-->
 <?php include 'includes/nav.php' ?>
 <!-- Masthead-->
-<div class="masthead">
-    <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
-        <div class="d-flex justify-content-center">
-            <div class="text-center">
-                <section class="projects-section bg-light" id="login">
-                    <table>
-                        <tr>
-                            <th>Username</th>
-                            <th>Password</th>
-                            <th>State</th>
-                            <th>Role</th>
-                            <th>Actions</th>
-                        </tr>
-                        <?php foreach ($db->get_users() as $user): ?>
-                        <tr>
-                            <td><?= $user['username'] ?></td>
-                            <td><?= $user['password'] ?></td>
-                            <td><?= $user['active'] == '1' ? 'Active' : 'Inactive' ?></td>
-                            <td><?= $user['role'] ?></td>
-                            <td>
-                                <a href="modifyUser.php?id=<?= $user['id'] ?>">Modify</a>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
-                </section>
+<main class="main is-visible" data-dropzone-area="">
+    <main class="container">
+        <div class="row align-items-center justify-content-center min-vh-100 gx-0">
+            <div class="d-flex justify-content-center">
+                <div class="text-center">
+                    <section class="projects-section bg-light" id="login">
+                        <table class="table">
+                            <tr>
+                                <th>Username</th>
+                                <th>Password</th>
+                                <th>State</th>
+                                <th>Role</th>
+                                <th>Actions</th>
+                            </tr>
+                            <?php foreach ($db->get_users() as $user): ?>
+                            <tr>
+                                <td><?= $user['username'] ?></td>
+                                <td><?= $user['password'] ?></td>
+                                <td><?= $user['active'] == '1' ? 'Active' : 'Inactive' ?></td>
+                                <td><?= $user['role'] ?></td>
+                                <td>
+                                    <a href="modifyUser.php?id=<?= $user['id'] ?>">Modify</a>
+                                </td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </table>
+                    </section>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</main>
 
 <?php
 require_once('includes/footer.php');
